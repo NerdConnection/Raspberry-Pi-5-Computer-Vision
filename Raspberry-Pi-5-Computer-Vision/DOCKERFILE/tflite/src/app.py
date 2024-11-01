d3addd4591fb64967598f5c54a12924404cd656c
bssfr123 <81203251+bssfr123@users.noreply.github.com>
2024-11-01 14:33:45 +0000

    Add files via upload

---
+++ Raspberry-Pi-5-Computer-Vision/DOCKERFILE/tflite/src/app.py
@@ -0,0 +1,232 @@
+import argparse
+import asyncio
+import json
+import logging
+import os
+import uuid
+import time
+import av
+import numpy as np
+import importlib.util
+from fractions import Fraction
+from picamera2 import Picamera2
+from aiohttp import web
+from aiortc import RTCPeerConnection, RTCSessionDescription
+from aiortc.contrib.media import MediaStreamTrack, MediaRelay
+import cv2
+import tflite_object_detection
+import subprocess
+import psutil  # 시스템 리소스 확인을 위한 psutil 모듈
+
+ROOT = os.path.dirname(__file__)
+ROOT_STATIC = os.path.join(os.path.dirname(__file__), "static")
+ROOT_TEMPLATES = os.path.join(os.path.dirname(__file__), "templates")
+
+cam = Picamera2()
+cam.configure(cam.create_video_configuration())
+cam.start()
+pcs = {}
+
+# TensorFlow Lite 모델 및 레이블 맵 초기화
+OBJECT_MODEL_DIR = "src/TFLite_model/object_detection/"
+LABELMAP_NAME = "labels.txt"  # COCO 데이터셋의 레이블 사용
+
+# 레이블 맵 로드
+with open(os.path.join("src", LABELMAP_NAME), 'r') as f:
+    labels = [line.strip() for line in f.readlines()]
+if labels[0] == '???':
+    del(labels[0])
+
+
+class PiCameraTrack(MediaStreamTrack):
+    kind = "video"
+
+    def __init__(self, transform):
+        super().__init__()
+        self.transform = transform  # 사용자 선택 모델
+
+        # 사용자가 선택한 모델 및 레이블 로드
+        if self.transform == "efficientdet":
+            self.model_name = 'efficientdet.tflite'
+        elif self.transform == "ssd-mobilenet-v1":
+            self.model_name = 'ssd-mobilenet-v1.tflite'
+        else:
+            raise ValueError(f"Unsupported model type: {self.transform}")
+        
+        # TensorFlow Lite 모델을 초기화 시에 로드
+        self.interpreter, self.input_details, self.output_details = tflite_object_detection.load_model(OBJECT_MODEL_DIR, self.model_name)
+
+    async def recv(self):
+        img = cam.capture_array()
+
+        # TensorFlow Lite 모델로 이미지 변환
+        processed_image = tflite_object_detection.transform_tflite(
+            img, self.interpreter, self.input_details, self.output_details, labels
+        )
+
+        # 이미지가 RGBA 형식인 경우 RGB로 변환
+        if processed_image.shape[2] == 4:
+            processed_image = cv2.cvtColor(processed_image, cv2.COLOR_RGBA2RGB)
+
+        # 새 VideoFrame 생성
+        new_frame = av.VideoFrame.from_ndarray(processed_image, format='rgb24')
+
+        # PTS(Presentation Time Stamp) 설정
+        pts = time.time() * 1000000
+        new_frame.pts = int(pts)
+        new_frame.time_base = Fraction(1, 1000000)
+
+        return new_frame
+
+
+async def on_connectionstatechange(pc_id, pc, model):
+    logging.info(f"Connection state for {pc_id} is {pc.connectionState}")
+    if pc.connectionState == "failed":
+        logging.error(f"Connection {pc_id} failed. Closing connection.")
+        
+        for sender in pc.getSenders():
+            if sender.track:
+                sender.track.stop()
+        
+        await pc.close()
+        pcs.pop(pc_id, None)
+        
+    elif pc.connectionState == "closed":
+        logging.info(f"Connection {pc_id} closed. Removing from pcs.")
+
+        for sender in pc.getSenders():
+            if sender.track:
+                sender.track.stop()
+
+        await pc.close()
+        pcs.pop(pc_id, None)
+
+    elif pc.connectionState == "connecting":
+        logging.info("connecting container 'tflite'")
+                
+
+efficientdet_track = PiCameraTrack(transform="efficientdet")
+mobilenet_track = PiCameraTrack(transform="ssd-mobilenet-v1")
+
+async def webrtc(request):
+    logging.info("Received WebRTC request")
+    try:
+        params = await request.json()
+        if params["type"] == "request":
+            pc = RTCPeerConnection()
+            pc_id = f"PeerConnection({uuid.uuid4()})"
+            pcs[pc_id] = pc
+            pc.on("connectionstatechange")(lambda: asyncio.create_task(on_connectionstatechange(pc_id, pc, params["video_transform"])))
+            
+            if params["video_transform"] == "efficientdet":
+                pc.addTrack(efficientdet_track)
+            elif params["video_transform"] == "ssd-mobilenet-v1":
+                pc.addTrack(mobilenet_track)
+            
+            offer = await pc.createOffer()
+            await pc.setLocalDescription(offer)
+
+            await asyncio.sleep(0)  # 다른 작업을 위해 대기
+
+            while pc.iceGatheringState != "complete":
+                await asyncio.sleep(0.1)
+
+            return web.Response(
+                content_type="application/json",
+                text=json.dumps({
+                    "sdp": pc.localDescription.sdp,
+                    "type": pc.localDescription.type,
+                    "id": pc_id,
+                    "iceServers": []
+                }),
+            )
+        elif params["type"] == "answer":
+            pc = pcs.get(params["id"])
+
+            if not pc:
+                return web.Response(
+                    content_type="application/json",
+                    text=json.dumps({"error": "PeerConnection not found"}),
+                    status=404,
+                )
+
+            await pc.setRemoteDescription(RTCSessionDescription(sdp=params["sdp"], type=params["type"]))
+
+            return web.Response(content_type="application/json", text=json.dumps({}))
+
+    except Exception as e:
+        logging.error(f"Error during WebRTC handling: {e}")
+        return web.Response(content_type="application/json", text=json.dumps({"error": str(e)}), status=500)
+
+
+async def on_shutdown(app):
+    coros = [pc.close() for pc in pcs.values()]
+    await asyncio.gather(*coros)
+    pcs.clear()
+
+# CPU 온도 가져오는 함수
+def get_cpu_temperature():
+    try:
+        with open("/sys/class/thermal/thermal_zone0/temp", "r") as f:
+            temp = float(f.read()) / 1000.0  # 섭씨로 변환
+        return temp
+    except Exception as e:
+        logging.error(f"Error reading CPU temperature: {e}")
+        return None
+
+# 시스템 리소스를 반환하는 엔드포인트
+async def get_system_resources(request):
+    cpu_temp = get_cpu_temperature()
+    memory = psutil.virtual_memory()
+    resources = {
+        'cpu_temp': cpu_temp,
+        'memory_total': memory.total,
+        'memory_used': memory.used,
+        'memory_free': memory.free,
+    }
+    return web.json_response(resources)
+
+async def return_ip(request):
+    data = await request.json()
+    # wlan0 인터페이스의 IP 주소 가져오기
+    result = subprocess.run(["ip", "addr", "show", "wlan0"], capture_output=True, text=True)
+    output = result.stdout
+    for line in output.splitlines():
+        if "inet " in line:
+            ip_address = line.split()[1].split('/')[0] 
+            break
+    else:
+        ip_address = None 
+        
+    logging.info(f"======== wlan0 IP: {ip_address} ========")
+    return web.json_response({"ip": ip_address})
+    
+async def index(request):
+    content = open(os.path.join(ROOT_TEMPLATES, "index.html"), "r").read()
+    return web.Response(content_type="text/html", text=content)
+
+
+async def javascript(request):
+    content = open(os.path.join(ROOT_STATIC, "client.js"), "r").read()
+    return web.Response(content_type="application/javascript", text=content)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Raspberry Pi WebRTC Camera Streamer")
+    parser.add_argument("--host", default="0.0.0.0", help="Host for HTTP server (default: 0.0.0.0)")
+    parser.add_argument("--port", type=int, default=80, help="Port for HTTP server (default: 8080)")
+    parser.add_argument("--verbose", "-v", action="count")
+
+    args = parser.parse_args()
+
+    logging.basicConfig(level=logging.DEBUG if args.verbose else logging.INFO)
+
+    app = web.Application()
+    app.on_shutdown.append(on_shutdown)
+    app.router.add_get("/", index)
+    app.router.add_get("/client.js", javascript)
+    app.router.add_post("/webrtc", webrtc)
+    app.router.add_post("/get-ip", return_ip)
+    app.router.add_get("/system_resources", get_system_resources)  # 시스템 리소스 엔드포인트 추가
+    logging.info(f"======== Running on http://{args.host}:{args.port} ========")
+    web.run_app(app, host=args.host, port=args.port)
---
+++ Raspberry-Pi-5-Computer-Vision/실증적 sw프로젝트/자료/README.md
@@ -0,0 +1,26 @@
+![Untitled](https://github.com/NerdConnection/Raspberry-Pi-5-Computer-Vision/assets/100738404/1d7b75f8-ce6c-47d9-b7e5-7c9a76811c98)
+
+### 프로젝트 순서
+
+1. 문제 정의 먼저 해야함
+    
+    (FairDay 발표 때 첫 마디로 할 말)
+    
+    → 이 기술과 프로젝트가 왜 필요한가
+    
+2. 문제를 해결하기 위한 기술 list up 
+    
+    ⇒ 해당 기술에 대한 reference 조사 
+    
+    → 문제점 찾고, 해결or성능 향상
+    
+3. ~ A, B, C 기술에 이런 장단점이 있어서 
+    
+    우리는 이렇게 개선하고 결합해서 
+    
+    구현or해결 했다.
+
+- 추가적으로 더 복잡한 문제 정의 필요
+    - RasPi의 접근성과 성능을 활용한 CV Benchmark로써의 활용
+    - 기존의 있는 컴퓨터 비전의 문제점을 보완?
+    - 여러 변수들의 고려를 위한 하드웨어 쪽으로의 개발
